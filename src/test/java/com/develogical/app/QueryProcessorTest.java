b87bef496b91e980f4539ab5a07771c9afb34158
sreeket <sreeket@gmail.com>
2018-07-02 14:33:50 +0100

    Test update

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -16,11 +16,11 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsAboutShakespeare() throws Exception {
-        assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("APE"), containsString("agile"));
     }
 
     @Test
     public void isNotCaseSensitive() throws Exception {
-        assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
+        assertThat(queryProcessor.process("ape"), containsString("agile"));
     }
 }
